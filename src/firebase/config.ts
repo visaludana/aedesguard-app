7c36802500321689cc21698661818313efd61002
Visal Udana <visaludana777@gmail.com>
2026-02-15 11:50:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1097369659-c3c8b",
+  "appId": "1:1031562972287:web:c9d6b02a8cf3d0eaaec34a",
+  "apiKey": "AIzaSyCyVE-qFgSIdTpKsvYxKyos_muO2j5ru8k",
+  "authDomain": "studio-1097369659-c3c8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1031562972287"
+};
